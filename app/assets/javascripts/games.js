a25cede4bd53f73b00e8d8c69172cf435f562ccf
Sofia Braun <sofi.braun@gmail.com>
2015-12-03 13:28:29 -0300

    alert if you won the game

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -6,10 +6,14 @@ $(function(){
     var url = $('#board').data("play-url");
     $.post(url, { "column": col } , function( data ) {
       if (data.status == 'ok') {
-        var lastRow = data.last_move[0];
-        var lastCol = data.last_move[1];
-        $("[data-col=" + lastCol + "][data-row=" + lastRow + "] div").addClass(data.player);
-        console.log('lastRow: ' + lastRow + ' lastCol: ' + lastCol);
+        if (data.game.winner_id) {
+          alert('you are the winner! :)');
+        } else {
+          var lastRow = data.last_move[0];
+          var lastCol = data.last_move[1];
+          $("[data-col=" + lastCol + "][data-row=" + lastRow + "] div").addClass(data.player);
+          console.log('lastRow: ' + lastRow + ' lastCol: ' + lastCol);
+        }
       } else {
         alert(data.message);
       }
