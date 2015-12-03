c42b4cd0f55d394d3d34ce6465da7a2dd7fdc3a1
Sofia Braun <sofi.braun@gmail.com>
2015-12-03 14:30:12 -0300

    last changes to game

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -4,17 +4,14 @@ $(function(){
     var el = $(e.currentTarget);
     var col = el.data("col");
     var url = $('#board').data("play-url");
-    console.log('column: ' + col);
     $.post(url, { "column": col } , function( data ) {
       if (data.status == 'ok') {
         if (data.game.winner_id) {
           alert('you are the winner! :)');
-        } else {
-          var lastRow = data.last_move[0];
-          var lastCol = data.last_move[1];
-          $("[data-col=" + lastCol + "][data-row=" + lastRow + "] div").addClass(data.player);
-          console.log('lastRow: ' + lastRow + ' lastCol: ' + lastCol);
-        }
+        } 
+        var lastRow = data.last_move[0];
+        var lastCol = data.last_move[1];
+        $("[data-col=" + lastCol + "][data-row=" + lastRow + "] div").addClass(data.player);
       } else {
         alert(data.message);
       }
